b0e3a1af0e6f4e52bd1213191df2959ed89695da
VLx2000 <victor.antunes.2000@gmail.com>
2020-10-15 19:02:48 -0300

    GOTO salvando o dia

--- implementacao2/teste.c
+++ implementacao2/teste.c
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define NUMERO_PESSOAS 12
+#define NUMERO_PESSOAS 22
 
 int lugares = 5;
 int cheio = 0;
@@ -18,6 +18,7 @@ int verifica_Cheio(){
     return esta_cheio;
 }
 
+//Solução q n eh justa, pois ordem de chegada n eh mantida.
 void *Sushi(void *thread)
 {
     int thread_id = *(int *)thread;
@@ -27,19 +28,24 @@ void *Sushi(void *thread)
     if (cheio)  //se o bar estiver cheio a pessoa vai para fila
     {
         printf("Pessoa \t%d\t entrou na fila.\n", thread_id + 1);
-        pthread_mutex_unlock(&trava);
+FILA:   pthread_mutex_unlock(&trava);
         while (verifica_Cheio())    //verificando se eh possivel entrar no bar
         {
             sleep(wait_sec);
             //printf("**Pessoa %d ainda esta na fila.\n", thread_id);
         }
         //duas pessoas ainda estao saindo da fila ao msm tempo
-        //possivel solução: uso de função
+        //possivel solução: uso de função ou goto :P
         pthread_mutex_lock(&trava);
     }
     lugares--;  //pessoa entra no restaurante
     if (lugares == 0)
         cheio = 1;  //se n houver mais lugares quer dizer q esta cheio
+    if (lugares < 0){
+        //printf("\nPessoa \t%d\t esta voltando para a fila.\n\n", thread_id + 1);
+        lugares++;
+        goto FILA;      //GOTO provisorio, mas q salva o dia
+    }
     printf("Pessoa \t%d\t esta comendo.\t\t\t(%d lugares vagos) CHEIO = %d\n", thread_id + 1, lugares, cheio);
     pthread_mutex_unlock(&trava);
 
