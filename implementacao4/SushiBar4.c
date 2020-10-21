fc9ef65123574c0d4635ced39ffb543e37b9ad1a
Rafael Murata <ryy.murata@gmail.com>
2020-10-21 19:32:09 -0300

    Refatorando o Código e Adicionando Comentários

--- implementacao4/SushiBar4.c
+++ implementacao4/SushiBar4.c
@@ -4,32 +4,31 @@
 #include <time.h>
 #include <unistd.h>
 
-pthread_mutex_t mutex;
-pthread_cond_t condicao;
-pthread_barrier_t barreira;
-int lugares_ocupados = 0;
-int numero_lugares;
+pthread_mutex_t mutex;          // mutex para garantir o controle das variáveis
+pthread_cond_t condicao;        // condição para controlar a entrada dos clientes
+pthread_barrier_t barreira;     // barreira controla a saída quando o bar está lotado
+int lugares_ocupados = 0;       // lugares OCUPADOS no bar
+int numero_lugares;             // Número total de lugares (assentos) no bar
 
-void *SushiBar(void* thread) {
-    int thread_id = *(int*) thread;
-    int wait_sec = rand() % 5 + 1;
-    
-    // Entrada dos Clientes:
+// Protótipos de Funções:
+void EntraCliente(int thread_id);
+void EsperaNaFila(int thread_id);
+void SaiCliente(int thread_id);
 
+
+/* 
+   Esta função recebe o id da thread como parâmetro e 
+   realiza a entrada dos clientes no bar 
+*/
+void EntraCliente(int thread_id) {
     pthread_mutex_lock(&mutex);
     
+    // Caso o bar esteja lotado:
     while (lugares_ocupados == numero_lugares) {
-
-        //sleep(wait_sec);
-        printf("Cliente %d esperando...\n", thread_id);        
-
-        pthread_cond_wait(&condicao, &mutex);
-
-        pthread_mutex_unlock(&mutex);
-
-        pthread_mutex_lock(&mutex);
+        EsperaNaFila(thread_id);
     }
 
+    // Entrada do Cliente
     lugares_ocupados++;
 
     if (lugares_ocupados > numero_lugares) {
@@ -39,50 +38,73 @@ void *SushiBar(void* thread) {
     printf("Cliente %d ENTROU\n", thread_id);    
 
     pthread_mutex_unlock(&mutex);
+}
 
-    sleep(wait_sec);
 
-    // Saída dos Clientes:
+/*
+   Esta função recebe o id da thread como parâmetro e 
+   coloca as threads em espera
+*/
+void EsperaNaFila(int thread_id) {
+    printf("Cliente %d esperando...\n", thread_id);        
 
+    pthread_cond_wait(&condicao, &mutex);       // Bloqueia as threads, esperando a liberação dos lugares no bar (lotado)   
+    pthread_mutex_unlock(&mutex);
+
+    pthread_mutex_lock(&mutex);
+}
+
+
+/* 
+   Esta função recebe o id da thread como parâmetro e 
+   realiza a saída do cliente do bar.
+   Se está lotado: Saem todos os clientes dentro do bar
+   Caso contrário: Cliente sai do bar normalmente 
+*/
+void SaiCliente(int thread_id) {
     pthread_mutex_lock(&mutex);
 
     if (lugares_ocupados == numero_lugares) {
         pthread_mutex_unlock(&mutex);
-        pthread_barrier_wait(&barreira);
+        pthread_barrier_wait(&barreira);        
         
-
         pthread_mutex_lock(&mutex);
         lugares_ocupados--;
+        
         printf("Cliente %d terminou de comer\n", thread_id);
-        sleep(1);
-        printf("Dentro de esta_lotado: Cliente %d SAIU\n", thread_id);
+        printf("Lotado: Cliente %d SAIU\n", thread_id);
+        
         pthread_mutex_unlock(&mutex);
 
-        pthread_barrier_wait(&barreira);
+        pthread_barrier_wait(&barreira);        // Barreira espera todos saírem do bar para sinalizar para novos clientes entrarem
 
         pthread_mutex_lock(&mutex);
-        pthread_cond_broadcast(&condicao);
+        pthread_cond_broadcast(&condicao);      // Sinaliza para as threads que o bar está vazio
     } else {
         lugares_ocupados--;
         printf("Cliente %d terminou de comer\n", thread_id);
-        sleep(1);
         printf("Não lotado: Cliente %d SAIU\n", thread_id);
-        pthread_mutex_unlock(&mutex);
-
-        pthread_mutex_lock(&mutex);
-        pthread_cond_broadcast(&condicao);
 
     }
-    
     pthread_mutex_unlock(&mutex);
-
 }
 
 
+// Função da Thread
+void *SushiBar(void* thread) {
+    int thread_id = *(int*) thread;
+    int wait_sec = rand() % 5 + 1;
 
+    EntraCliente(thread_id);
+    sleep(wait_sec);
+    SaiCliente(thread_id);
+}
+
+
+// Função Principal
 int main(int argc, char *argv[]) {
 
-    int numero_clientes = 12;                   // Número de Clientes Padrão
+    int numero_clientes = 12;                   // Número Padrão de Clientes 
     numero_lugares = 5;                         // Número Padrão de Lugares no Bar
 
     // Recebendo Parâmetros por Linha de Comando
@@ -96,7 +118,7 @@ int main(int argc, char *argv[]) {
 
     pthread_t threads[numero_clientes];
     unsigned int thread_ids[numero_clientes];
-    int status;                                 // Checa o status de cada pthread (sucesso / falha)
+    int status;                                 // Checa o status de criação de cada pthread (sucesso / falha)
     srand(time(NULL));
 
     // Inicializações:
